e190775ddd081f431d9f91a18cc13ea10539754e
pedroanastacio <pedro.anastacio013@gmail.com>
2020-11-21 09:48:28 -0300

    add filtros

--- SellPhones.Celulares.Web/App_Start/RouteConfig.cs
+++ SellPhones.Celulares.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace SellPhones.Celulares.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "FilterByName",
+               url: "Marcas/FilterByName/{search}",
+               defaults: new { controller = "Marcas", action = "FilterByName", search = UrlParameter.Optional }
+               );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
